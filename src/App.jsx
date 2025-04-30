49333bcbba54091f3ad242a6f27840a96b1f2f04
TanyaSoni29 <tanu139sona@gmail.com>
2025-04-30 23:18:30 +0530

    neccesary files and code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import Homepage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -8,7 +12,11 @@ function App() {
       {/* if you want any header or navbar we can write here this remain same in all other pages */}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/app" element={<AppLayout />} />
           {/* this star path route met when all defined route path not match then this Page not Found route will be displayed */}
           <Route path="*" element={<PageNotFound />} />
         </Routes>
@@ -19,6 +27,6 @@ function App() {
 
 export default App;
 
-// why we use Link or NavLink for moving between different page not anchor tag because when we use anchor tag then whole page is reloaded means hard reloading occur which should not happened in single page application...in single page application dom is updated with help of javascript without any reload of whole page.... 
+// why we use Link or NavLink for moving between different page not anchor tag because when we use anchor tag then whole page is reloaded means hard reloading occur which should not happened in single page application...in single page application dom is updated with help of javascript without any reload of whole page....
 
 // <Link to="/pageName" /> before pageName we use slash which is telling the application to start with base url then move to that page...
